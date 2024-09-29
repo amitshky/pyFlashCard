2eceea01d4b470aa4bb816ebf17f804ed0844cfe
amitshky <amitshkymail@gmail.com>
2024-09-29 19:36:25 +0545

    vocabs update; convert txt to yml added

--- src/utils.py
+++ src/utils.py
@@ -10,3 +10,17 @@ def wrap_meanings(meaning_string: list[str]) -> list[str]:
         meaning = f"{i+1}. {meaning.strip()}"
         meanings.extend(wrapper.wrap(meaning))
     return meanings
+
+
+# converts txt files (should be of proper format, see README.md) to yml files
+# @params in_file - input file (should include extenstion (ie .txt))
+# @params out_file - output file (should include extenstion (ie .yml))
+def txt_to_yml(in_file: str, out_file: str):
+    with open(in_file) as file:
+        data = ""
+        for line in file:
+            split = line.split(" - ")
+            data += f"- word: {split[0]}\n  definition: '{split[1].replace('\n', '')}'\n  language: en\n"
+
+    with open(out_file, 'w+') as file:
+        file.write(data)
